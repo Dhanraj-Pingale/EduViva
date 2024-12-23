85f0938c659718ab4554e2e36ae11d5c9eb181ae
Dhanraj Pingale <pingaledhanraj15@gmail.com>
2024-12-23 15:36:33 +0530

    req

--- client/src/components/user_homepage/User_homepage.jsx
+++ client/src/components/user_homepage/User_homepage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Outlet } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home1";
 import { useLocation } from "react-router-dom";
 
 export default function UserDashboard() {
